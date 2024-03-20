853ae485977fd330fe82e47b2171f86a98b96ccc
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-03-20 15:59:13 +0200

    puhelinluettelo backend step8

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const PORT = 3001
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body)
+    ].join(' ')
+  }))
 
 let numbers = [
     {
