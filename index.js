73541fca4ffdd7ce82db5749fcfc2104d685acef
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-03-20 17:14:50 +0200

    puhelinluettelo backend step9

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(morgan(function (tokens, req, res) {
       tokens['response-time'](req, res), 'ms',
       JSON.stringify(req.body)
     ].join(' ')
-  }))
+}))
+app.use(express.static('dist'))
 
 let numbers = [
     {
